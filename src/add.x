b0e85477a0f224dfb1f32720affa928527504e90
Patrick DeCoster <patrickmd@protonmail.com>
2023-11-03 18:51:42 -0400

    rpc

---
+++ src/add.x
@@ -0,0 +1,10 @@
+struct numbers {
+    int a;
+    int b;
+};
+
+program ADDPROG {
+    version ADDVERS {
+        int add(numbers) = 1;
+    }=1;
+}=0x22331111;
--- src/add_svc.c
+++ src/add_svc.c
@@ -3,7 +3,7 @@
  * It was generated using rpcgen.
  */
 
-#include "msg.h"
+#include "add.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <rpc/pmap_clnt.h>
@@ -17,10 +17,10 @@
 #endif
 
 static void
-messageprog_1(struct svc_req *rqstp, register SVCXPRT *transp)
+addprog_1(struct svc_req *rqstp, register SVCXPRT *transp)
 {
 	union {
-		char *printmessage_1_arg;
+		numbers add_1_arg;
 	} argument;
 	char *result;
 	xdrproc_t _xdr_argument, _xdr_result;
@@ -31,10 +31,10 @@ messageprog_1(struct svc_req *rqstp, register SVCXPRT *transp)
 		(void) svc_sendreply (transp, (xdrproc_t) xdr_void, (char *)NULL);
 		return;
 
-	case PRINTMESSAGE:
-		_xdr_argument = (xdrproc_t) xdr_wrapstring;
+	case add:
+		_xdr_argument = (xdrproc_t) xdr_numbers;
 		_xdr_result = (xdrproc_t) xdr_int;
-		local = (char *(*)(char *, struct svc_req *)) printmessage_1_svc;
+		local = (char *(*)(char *, struct svc_req *)) add_1_svc;
 		break;
 
 	default:
@@ -62,15 +62,15 @@ main (int argc, char **argv)
 {
 	register SVCXPRT *transp;
 
-	pmap_unset (MESSAGEPROG, MESSAGEVERS);
+	pmap_unset (ADDPROG, ADDVERS);
 
 	transp = svcudp_create(RPC_ANYSOCK);
 	if (transp == NULL) {
 		fprintf (stderr, "%s", "cannot create udp service.");
 		exit(1);
 	}
-	if (!svc_register(transp, MESSAGEPROG, MESSAGEVERS, messageprog_1, IPPROTO_UDP)) {
-		fprintf (stderr, "%s", "unable to register (MESSAGEPROG, MESSAGEVERS, udp).");
+	if (!svc_register(transp, ADDPROG, ADDVERS, addprog_1, IPPROTO_UDP)) {
+		fprintf (stderr, "%s", "unable to register (ADDPROG, ADDVERS, udp).");
 		exit(1);
 	}
 
@@ -79,8 +79,8 @@ main (int argc, char **argv)
 		fprintf (stderr, "%s", "cannot create tcp service.");
 		exit(1);
 	}
-	if (!svc_register(transp, MESSAGEPROG, MESSAGEVERS, messageprog_1, IPPROTO_TCP)) {
-		fprintf (stderr, "%s", "unable to register (MESSAGEPROG, MESSAGEVERS, tcp).");
+	if (!svc_register(transp, ADDPROG, ADDVERS, addprog_1, IPPROTO_TCP)) {
+		fprintf (stderr, "%s", "unable to register (ADDPROG, ADDVERS, tcp).");
 		exit(1);
 	}
 
